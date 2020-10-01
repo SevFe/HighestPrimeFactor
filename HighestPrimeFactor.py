d205c9dab22af833e0eeb3f3c3647a0a1044ab2c
SevFe <62662157+SevFe@users.noreply.github.com>
2020-10-01 15:12:19 +0300

    Create HighestPrimeFactor.py

---
+++ HighestPrimeFactor.py
@@ -0,0 +1,22 @@
+numberGiven = int(input("Input a number: "))
+
+def primeFactor(x):
+
+  if x <= 1:
+    print("Not Valid")
+    return False
+
+  numberHolder = 0
+
+  while x % 2 == 0:
+    numberHolder = 2
+    x = x//2
+
+  for i in range(3, x+1, 2):
+    while x % i == 0:
+      numberHolder = i
+      x = x//i
+
+  print("The highest prime factor is:", numberHolder)
+
+primeFactor(numberGiven)
